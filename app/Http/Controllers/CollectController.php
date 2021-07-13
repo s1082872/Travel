3527124c4ed0533654586fbcfce0b8d7ea67800e
Eveon <uuu9147096@gmail.com>
2021-07-13 09:35:07 +0800

    first test

---
+++ app/Http/Controllers/CollectController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\collect;
+use Illuminate\Http\Request;
+
+class CollectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\collect  $collect
+     * @return \Illuminate\Http\Response
+     */
+    public function show(collect $collect)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\collect  $collect
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(collect $collect)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\collect  $collect
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, collect $collect)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\collect  $collect
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(collect $collect)
+    {
+        //
+    }
+}
